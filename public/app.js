3c894378fff004ba68f05f2c89c0bdc9f8cd6e9d
Kyle Friel <businessfriel@gmail.com>
2021-06-10 16:59:00 -0400

    Setup slideshow JS

--- public/app.js
+++ public/app.js
@@ -1,7 +1,27 @@
-// const main = () => {
-//     const
-// }
+const slideshow = async () => {
+    const img = document.getElementById("img");
+    const leftArrow = document.getElementById("leftArrow");
+    const rightArrow = document.getElementById("rightArrow");
+
+    const toggleSlideshow = (i) => {
+        i++;
+        if (i === 8) i = 1;
+        if (i === 0) i = 7;
+        img.src = `./../public/images/img-${i}.jpg`;
+        return i;
+    };
+    leftArrow.addEventListener("click", () => {
+        slideIndex = toggleSlideshow(slideIndex--);
+    });
+    rightArrow.addEventListener("click", () => {
+        slideIndex = toggleSlideshow(slideIndex++);
+    });
+    let slideIndex = toggleSlideshow(0);
+};
 
 window.addEventListener("load", () => {
     main();
 });
+const main = () => {
+    slideshow();
+};
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use("/public", express.static(`${__dirname}/public`));
+
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get("/api/imgcount", (req, res) => {
+    const files = fs.readdirSync(`${__dirname}/public/images/`);
+    res.json(files);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
