70a94a3b9999adbcd5cb8d4fee17c94550ede852
Kyle Friel <businessfriel@gmail.com>
2021-06-10 14:05:38 -0400

    Added node server

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use("/public", express.static(`${__dirname}/public`));
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log(`App listening on port ${listener.address().port}`);
+});
